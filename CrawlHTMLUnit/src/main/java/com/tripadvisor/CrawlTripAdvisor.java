5eb5d80809f82d408ef150fd14eeaedbb214d581
Rahul Kapoor <rahulkapoor.bits@gmail.com>
2014-03-24 00:59:16 +0530

    Changed pom.xml

--- CrawlHTMLUnit/src/main/java/com/tripadvisor/CrawlTripAdvisor.java
+++ CrawlHTMLUnit/src/main/java/com/tripadvisor/CrawlTripAdvisor.java
@@ -4,6 +4,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.logging.Level;
 
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
