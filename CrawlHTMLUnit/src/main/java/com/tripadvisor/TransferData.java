edf7fa65389932580ce7c424e062a959f1d27996
Rahul Kapoor <rahulkapoor.bits@gmail.com>
2014-04-12 03:08:50 +0530

    insertions

--- CrawlHTMLUnit/src/main/java/com/tripadvisor/TransferData.java
+++ CrawlHTMLUnit/src/main/java/com/tripadvisor/TransferData.java
@@ -26,7 +26,7 @@ public class TransferData {
 		String name="Bull Temple";
 		String address= "Bugle Hill, Bull Temple Rd, Basavangudi, Bangalore, India";
 		String pincode = "120021";
-		String phone = "unknown";
+		String phone = "";
 		String ranktext= "Ranked #24 of 181 attractions in Bangalore";
 		String rating= "4.0";
 		String numofreviews= "133";
@@ -34,11 +34,16 @@ public class TransferData {
 		Boolean isCoE = false;
 		String type = "Architectural Buildings, Religious Sites";
 		String durValue = "1";
-		String Fee= "No";
+		String Fee= "Yes";
 		String description= "Located in Basavanagudi, this temple (built by Kempegowda in the Dravidian style) contains a huge granite monolith of Nandi. The temple grounds also host the annual groundnut fair in November/December. The nearby Dodda Ganesha Temple and Bugle Rock Garden also can be visited.";
 		String photolink= "http://media-cdn.tripadvisor.com/media/photo-s/01/1f/a2/0b/bangalore.jpg";
-		int CityID=-1;
+		int CityID=-1, PlaceID=-1;
 
+		city = city.toUpperCase();
+		state = state.toUpperCase();
+		country = country.toUpperCase();
+		name = name.toUpperCase();
+		type = type.toUpperCase();
 		//Inserting the City
 		//Checking if the City Exists in DB
 		ResultSet getCityR = statement.executeQuery("SELECT * FROM City WHERE CityName='"+city+"';");
@@ -84,6 +89,7 @@ public class TransferData {
 		{
 			if(CityID==getPlaceR.getInt("CityID"))
 			{
+				PlaceID=getPlaceR.getInt("PlaceID");
 				//The same name place in same city exists
 				placeExists = true;
 				if(getPlaceR.getInt(source)==1)
@@ -101,11 +107,38 @@ public class TransferData {
 					String update = "UPDATE Places SET TripAdvisor = 1";
 					
 					//Appending the type to the existing type
-					type = getPlaceR.getString("Type") +","+ type;
+					String typeOld = getPlaceR.getString("Type");
+					String typeNew = typeOld;
+					if(!type.isEmpty())
+					{
+						if(typeOld != null)
+						{
+							StringTokenizer typeOldt = new StringTokenizer(typeOld, ",");
+							StringTokenizer typet = new StringTokenizer(type, ",");
+							while(typet.hasMoreTokens())
+							{
+								String typeS = typet.nextToken().trim();
+								while(typeOldt.hasMoreTokens())
+								{
+									String typeOS = typeOldt.nextToken();
+									if(!typeS.equals(typeOS))
+									{
+										typeNew = typeNew + typeS;
+									}
+								}
+							}
+						}
+						update = update + ", Type = '"+typeNew+"'";
+					}
+					//Has to be replaced with mapping to our categories
 					
 					//Adding new address if old address doesn't contain digits
 					String addressOld = getPlaceR.getString("Address");
-					if(!addressOld.matches(".*\\d.*"))
+					if(addressOld==null)
+					{
+						update = update + ", Address = "+address;
+					}
+					else if(!addressOld.matches(".*\\d.*"))
 					{
 						if(address.matches(".*\\d.*"))
 						{
@@ -114,7 +147,7 @@ public class TransferData {
 					}
 					
 					//Adding Pincode if null
-					if(getPlaceR.getString("Pincode").isEmpty())
+					if(getPlaceR.getString("Pincode")==null)
 					{
 						update = update +", Pincode = "+pincode;
 					}
@@ -122,49 +155,78 @@ public class TransferData {
 					//Adding Phone Number if not same					 
 					String phoneOld = getPlaceR.getString("PhoneNo");
 					String phoneNum = phoneOld;
-					StringTokenizer stOld=new StringTokenizer(phoneOld, ",");
-					StringTokenizer stNew=new StringTokenizer(phone, ",");
-					while(stNew.hasMoreTokens())
+					if(phoneOld==null&&!phone.isEmpty())
 					{
-						String numNew = stNew.nextToken();
-						while(stOld.hasMoreTokens())
+						addNum = true;
+						phoneNum = phone;
+					}
+					else if(phoneOld!=null)
+					{
+						StringTokenizer stOld=new StringTokenizer(phoneOld, ",");
+						StringTokenizer stNew=new StringTokenizer(phone, ",");
+						while(stNew.hasMoreTokens())
 						{
-							if(numNew.equals(stOld.nextToken()))
+							String numNew = stNew.nextToken();
+							while(stOld.hasMoreTokens())
 							{
-								numExists = true;
+								if(numNew.equals(stOld.nextToken()))
+								{
+									numExists = true;
+								}
 							}
+							if(!numExists)
+							{
+								phoneNum = phoneNum + "," + numNew;
+								addNum = true;
+							}
+							numExists = false;
 						}
-						if(!numExists)
-						{
-							phoneNum = phoneNum + "," + numNew;
-							addNum = true;
-						}
-						numExists = false;
 					}
 					if(addNum)
 					{
 						update = update + ", PhoneNo = "+ phoneNum;
 					}
 					
-					
+					//Appending the description separated by $
 					String descOld = getPlaceR.getString("Description");
-					description = descOld + ":" + description;
-					update = update + ", Description = "+ description;
-					
-					double scoreOld = getPlaceR.getDouble("Score");
-					int scorenum = getPlaceR.getInt("Score");
-					if(scorenum!=0)
+					if(descOld!=null)
 					{
-						//score = (Integer.parseInt(rating)+ratingOld)/2;
+						if(!descOld.contains(description))
+						{
+							description = descOld + "$" + description;
+							update = update + ", Description = '"+ description+"'";
+						}
+					}
+					else
+					{
+						update = update + ", Description = '"+ description+"'";
 					}
 					
+					//Generating the score for the place and taking a mean with old score
+					double score;
+					double scoreNew = generateScore(ranktext, rating, numofreviews);
+					double scoreOld = getPlaceR.getDouble("Score");
+					int scorenum = getPlaceR.getInt("ScoreSources");
+					if(scorenum!=0)
+					{
+						scorenum++;
+						score = (scoreOld + scoreNew)/scorenum;
+						update = update + ", ScoreSources = "+ scorenum; 
+					}
+					else
+					{
+						score = scoreNew;
+					}
+					update = update + ", Score = "+ score;
 					
 					
-					statement.executeUpdate("UPDATE City SET TripAdvisor = 1 WHERE CityID = "+getCityR.getString("CityID")+";");
+					
+					System.out.println(update +" WHERE PlaceID = "+PlaceID+";");
+					//statement.executeUpdate(update +" WHERE PlaceID = "+getPlaceR.getString("PlaceID")+";");
 					break;
 					
 				}
-				//Merge the info already present
+				
 				
 			}
 			
@@ -172,9 +234,149 @@ public class TransferData {
 		if(!placeExists)
 		{
 			//Insert the data
-			statement.executeUpdate("INSERT INTO City(CityName,State,Country,TripAdvisor) VALUES('"+city+"','"+state+"','"+country+"',1);");
+			String insert= "INSERT INTO Places(";
+			String values= ") VALUES(";
+			insert = insert + "Name, CityID, TripAdvisor";
+			values = values + "'"+name+"', "+CityID+", 1";
+			if(!type.isEmpty())
+			{
+				insert = insert + ", Type";
+				values = values + ", '"+type+"'";
+			}
+			if(!address.isEmpty())
+			{
+				insert = insert + ", Address";
+				values = values + ", '"+address+"'";
+			}
+			if(!phone.isEmpty())
+			{
+				insert = insert + ", PhoneNo";
+				values = values + ", '"+phone+"'";
+			}
+			
+			if(!pincode.isEmpty())
+			{
+				insert = insert + ", PinCode";
+				values = values + ", '"+pincode+"'";
+			}
+			if(!description.isEmpty())
+			{
+				insert = insert + ", Description";
+				values = values + ", '"+description+"'";
+			}
+			if(!ranktext.isEmpty()&&!rating.isEmpty()&&!numofreviews.isEmpty())
+			{
+				double scoreNew = generateScore(ranktext, rating, numofreviews);
+				insert = insert + ", Score, ScoreSources";
+				values = values + ", "+scoreNew+", 1";
+			}
+			//System.out.println(insert + values+");");
+			statement.executeUpdate(insert + values+");");
+		    ResultSet rs = statement.getGeneratedKeys();
+		    rs.next();
+		    PlaceID = rs.getInt(1);
 		}
 		
+		//Insert the Image URL
+		if(!photolink.isEmpty())
+		{
+			ResultSet imgRS = statement.executeQuery("SELECT * FROM Place_Image WHERE ImgURL='"+photolink+"';");
+			if(!imgRS.next())
+			{
+				statement.executeUpdate("INSERT INTO Place_Image(PlaceID, ImgURL) VALUES("+PlaceID+",'"+photolink+"');");
+			}
+		}
+		
+		//Insert the Best Collection of Places
+		if(isCoE||isTravellersChoice)
+		{
+			ResultSet bestRS = statement.executeQuery("SELECT * FROM BestPlaces WHERE PlaceID="+PlaceID+";");
+			if(!bestRS.next())
+			{
+				statement.executeUpdate("INSERT INTO BestPlaces(PlaceID) VALUES("+PlaceID+";");
+			}
+		}
+		
+		//Insert the place charges
+		if(!Fee.isEmpty())
+		{
+			int fee=-1;
+			if(Fee.equalsIgnoreCase("Yes"))
+			{
+				fee=-2;
+			}
+			else if(Fee.equalsIgnoreCase("No"))
+			{
+				fee=0;
+			}
+			ResultSet feeRS = statement.executeQuery("SELECT * FROM Place_Charges WHERE PlaceID="+PlaceID+";");
+			if(feeRS.next())
+			{
+				boolean change=false;
+				String update= "UPDATE Place_Charges SET ";
+				if(feeRS.getInt("ChildCharge")==-1)
+				{
+					if(change)
+					{
+						update = update + ", ";
+					}
+					change=true;
+					update = update + "ChildCharge = "+fee;
+				}
+				if(feeRS.getInt("AdultCharge")==-1)
+				{
+					if(change)
+					{
+						update = update + ", ";
+					}
+					change=true;
+					update = update + "AdultCharge = "+fee;
+				}
+				if(feeRS.getInt("ForeignerCharge")==-1)
+				{
+					if(change)
+					{
+						update = update + ", ";
+					}
+					change=true;
+					update = update + "ForeignerCharge = "+fee;
+				}
+				if(change)
+				{
+					statement.executeUpdate(update + " WHERE PlaceID = "+PlaceID+";");
+				}
+			}
+			else
+			{
+				statement.executeUpdate("INSERT INTO Place_Charges(PlaceID, ChildCharge, AdultCharge, ForeignerCharge) VALUES("+PlaceID+", "+fee+", "+fee+", "+fee+")");
+			}
+		}
+		
+		//Insert the duration
+		if(!durValue.isEmpty())
+		{
+			if(durValue.matches("\\d+"))
+			{
+				int dur = Integer.parseInt(durValue);
+				ResultSet durRS = statement.executeQuery("SELECT * FROM Timing WHERE PlaceID="+PlaceID+";");
+				if(durRS.next())
+				{
+					double durOld = durRS.getDouble("ExpectedDuration");
+					if(durOld == -1)
+					{
+						statement.executeUpdate("UPDATE Timing SET ExpectedDuration = "+dur+" WHERE PlaceID = "+PlaceID+";");
+					}
+					else
+					{
+						statement.executeUpdate("UPDATE Timing SET ExpectedDuration = "+((durOld+dur)/2)+" WHERE PlaceID = "+PlaceID+";");
+					}
+				}
+				else
+				{
+					statement.executeUpdate("INSERT INTO Timing(PlaceID, ExpectedDuration) VALUES("+PlaceID+", "+dur+");");
+				}
+			}
+		}
 		
 		/*
 		String query = "SELECT * FROM Places;";
@@ -192,6 +394,51 @@ public class TransferData {
 		*/
 	
 	
+	}
+
+	private static double generateScore(String ranktext, String rating,	String numofreviews) throws NumberFormatException {
+		int rank=-1, totalplaces = -1, numofreviewsI = -1;
+		double score1=-1, score2=-1, ratingD=-1, wt=-1, scoreNew=-1;
+		//Checking if ranktext is in proper format
+		if(ranktext.matches("Ranked #\\d+ of \\d+ attractions in.*"))
+		{
+			int pos = ranktext.indexOf('#');
+			int pos1 = ranktext.indexOf(' ',pos);
+			rank = Integer.parseInt(ranktext.substring(pos+1, pos1));
+			
+			pos=ranktext.indexOf(' ',pos1+1);
+			pos1=ranktext.indexOf(' ', pos+1);
+			totalplaces=Integer.parseInt(ranktext.substring(pos+1, pos1));
+		}
+		if(rating.matches("\\d+.*"))
+		{
+			ratingD = Double.parseDouble(rating);
+			if(numofreviews.matches("\\d+"))
+			{
+				numofreviewsI = Integer.parseInt(numofreviews);  
+			}
+		}
+		if(rank!=-1&&totalplaces!=-1)
+		{
+			score1=(totalplaces-rank+1)/(double)totalplaces*100;
+		}
+		if(ratingD!=-1 &&numofreviewsI!=-1)
+		{
+			score2=ratingD*20;
+			wt=	Math.atan((Math.pow(numofreviewsI-250.0,3)/Math.pow(250.0,3)))*(70/Math.PI)+40;
+		}
+		if(score1!=-1)
+		{
+			if(score2!=-1&&wt!=-1)
+			{
+				scoreNew =(score2 * wt / 100) + (score1 * (100 - wt) / 100);
+			}
+			else
+			{
+				scoreNew = score1;
+			}
+		}
+		return scoreNew;
 	}			
 			
 }
