84480f10e7ed7370ae9d735e2aa50f3516c1d539
Rahul Kapoor <rahulkapoor.bits@gmail.com>
2014-10-25 16:51:15 +0530

    connectivity query

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -26,4 +26,16 @@ TRUNCATE TABLE City;
 TRUNCATE TABLE BestPlaces;
 
 
-INSERT INTO Places(Type,Name,PlaceID,Address,PinCode,PhoneNo,CityID,Description,Rating,Website,Latitude,Longitude,Ixigo,TripAdvisor)VALUES('Adventure','Rishikesh Tents','3','Near Haridwar','305004','2145698','22','blahblah','4.5','www.yoyo.com,'80.80','78.89','1','0');
+--INSERT INTO Places(Type,Name,PlaceID,Address,PinCode,PhoneNo,CityID,Description,Rating,Website,Latitude,Longitude,Ixigo,TripAdvisor)VALUES('Adventure','Rishikesh Tents','3','Near Haridwar','305004','2145698','22','blahblah','4.5','www.yoyo.com,'80.80','78.89','1','0');
+
+SELECT c.CityIDOrigin, c.CityIDDestination, IF(AirConnectivity IS NULL,0,AirConnectivity) as AirConnectivity, IF(BusConnectivity IS NULL,0,BusConnectivity) as BusConnectivity, IF(RailwayConnectivity IS NULL,0,RailwayConnectivity) as RailwayConnectivity FROM
+(SELECT CityIDOrigin, CityIDDestination FROM
+(SELECT CityID as CityIDOrigin FROM City WHERE CityName IN ('COORG', 'OOTY', 'BANGALORE', 'NEW DELHI', 'HYDERABAD')) a
+JOIN
+(SELECT CityID as CityIDDestination FROM City WHERE CityName IN ('COORG', 'OOTY', 'BANGALORE', 'NEW DELHI', 'HYDERABAD')) b) c
+LEFT OUTER JOIN
+(SELECT CityIDOrigin, CityIDDestination, NormalizedConnectivity AS AirConnectivity FROM City_Connectivity_Between_Air) d ON (c.CityIDOrigin = d.CityIDOrigin) AND (c.CityIDDestination = d.CityIDDestination)
+LEFT OUTER JOIN 
+(SELECT CityIDOrigin, CityIDDestination, NormalizedConnectivity AS BusConnectivity FROM City_Connectivity_Between_Bus) e ON (c.CityIDOrigin = e.CityIDOrigin) AND (c.CityIDDestination = e.CityIDDestination)
+LEFT OUTER JOIN 
+(SELECT CityIDOrigin, CityIDDestination, NormalizedConnectivity AS RailwayConnectivity FROM City_Connectivity_Between_Railway) f ON (c.CityIDOrigin = f.CityIDOrigin) AND (c.CityIDDestination = f.CityIDDestination)
--- TripPlannerApp/lib/getConnectivity.js
+++ TripPlannerApp/lib/getConnectivity.js
@@ -5,7 +5,21 @@ function getConnectivity(cities, callback)
 	console.log("getConnectivity called");
 	var connection=conn.conn();
 	connection.connect();
-	queryString = '';
+	var citiesString = '';
+	citiesString += ' \''+cities.split('\', \'')+'\' ';
+	console.log(citiesString);
+	queryString = 'SELECT c.CityIDOrigin, c.CityIDDestination, IF(AirConnectivity IS NULL,0,AirConnectivity) as AirConnectivity, IF(BusConnectivity IS NULL,0,BusConnectivity) as BusConnectivity, IF(RailwayConnectivity IS NULL,0,RailwayConnectivity) as RailwayConnectivity FROM'
+		+' (SELECT CityIDOrigin, CityIDDestination FROM'
+		+' (SELECT CityID as CityIDOrigin FROM City WHERE CityName IN ('+citiesString+')) a'
+		+' JOIN'
+		+' (SELECT CityID as CityIDDestination FROM City WHERE CityName IN ('+citiesString+')) b) c'
+		+' LEFT OUTER JOIN'
+		+' (SELECT CityIDOrigin, CityIDDestination, NormalizedConnectivity AS AirConnectivity FROM City_Connectivity_Between_Air) d ON (c.CityIDOrigin = d.CityIDOrigin) AND (c.CityIDDestination = d.CityIDDestination)'
+		+' LEFT OUTER JOIN' 
+		+' (SELECT CityIDOrigin, CityIDDestination, NormalizedConnectivity AS BusConnectivity FROM City_Connectivity_Between_Bus) e ON (c.CityIDOrigin = e.CityIDOrigin) AND (c.CityIDDestination = e.CityIDDestination)'
+		+' LEFT OUTER JOIN' 
+		+' (SELECT CityIDOrigin, CityIDDestination, NormalizedConnectivity AS RailwayConnectivity FROM City_Connectivity_Between_Railway) f ON (c.CityIDOrigin = f.CityIDOrigin) AND (c.CityIDDestination = f.CityIDDestination);';
+	console.log(queryString);
 	connection.query(queryString, function(err, rows, fields) {
 		if (err)
 		{
@@ -13,10 +27,10 @@ function getConnectivity(cities, callback)
 		}
 	    else{
 	    	for (var i in rows) {
-	    		console.log(rows[i]);
+	    		console.log(rows[i].CityIDOrigin+','+rows[i].CityIDDestination+','+rows[i].AirConnectivity','+rows[i].BusConnectivity','+rows[i].RailwayConnectivity);
 	    	}
 	    }
-	callback(rows);
+	callback(null, );
 	});
 	connection.end();
 }
