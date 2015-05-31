df3dae2447266325f4266fa3c8032973c57ebb72
Rahul Kapoor <rahulkapoor.bits@gmail.com>
2015-05-31 18:12:18 +0530

    Editing db connection

--- TripPlannerApp/lib/database.js
+++ TripPlannerApp/lib/database.js
@@ -5,14 +5,14 @@
 function conn()
 {
 var mysql = require('mysql');
- 
+
 var connection = mysql.createConnection(
     {
-      host     : '127.0.0.1',
+      host     : 'singapore-mysql-instance1.cp4adwqmzvxv.ap-southeast-1.rds.amazonaws.com',
       port     : '3306',
-      user     : 'adminzESTl5F',
-      password : 'duKiwg4kLMSV',
-      database : 'Holiday',
+      user     : 'adminTripsonair1',
+      password : 'adminDB!master',
+      database : 'Holiday'
     }
 );
 
@@ -26,14 +26,14 @@ var queryString = 'SELECT * FROM Places';
 
 connection.query(queryString, function(err, rows, fields) {
     if (err) throw err;
- 
+
     else{
-    
+
     	for (var i in rows) {
         console.log('Name: ', rows[i].Name);
     }
-    
+
     }
 });
- 
+
 connection.end();*/
